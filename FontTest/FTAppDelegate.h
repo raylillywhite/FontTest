ce853092e8d57eabb4575c035011c06fdf86ef97
Ray Lillywhite <ray.lillywhite@hoteltonight.com>
2011-12-06 14:31:17 -0800

    Initial Commit

---
+++ FontTest/FTAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  FTAppDelegate.h
+//  FontTest
+//
+//  Created by Ray Lillywhite on 12/6/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class FTViewController;
+
+@interface FTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) FTViewController *viewController;
+
+@end
---
+++ FontTest/FTViewController.h
@@ -0,0 +1,13 @@
+//
+//  FTViewController.h
+//  FontTest
+//
+//  Created by Ray Lillywhite on 12/6/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FTViewController : UIViewController
+
+@end
